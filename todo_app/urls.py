fcb4aff341cfcaeaff8ef1bdc25b3363edccb0c1
AlfrinP <alfrinpoulose@gmail.com>
2024-05-05 20:06:18 +0530

    just installed jwt and rest framwork

---
+++ todo_app/urls.py
@@ -0,0 +1,12 @@
+from django.db import router
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+
+from todo_app import views
+
+
+urlpatterns = [
+    path('', views.index,name="index"),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+]
